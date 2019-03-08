1506467cb6e01db07cd746f34924df79426acd5d
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-08 12:18:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,6 @@ def fourth_challenge
         #your hashes here!
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-
       ]
    },
    :capulet => {
